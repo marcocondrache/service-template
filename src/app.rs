fc0aa57416873d12c482c88ccfd61365cc8f56f0
Marco Mihai Condrache <52580954+marcocondrache@users.noreply.github.com>
2026-05-24 12:02:37 +0200

    fix(app): Inline index function

--- src/app.rs
+++ src/app.rs
@@ -3,9 +3,7 @@ mod health;
 use axum::{Router, http::StatusCode, routing::any};
 
 pub fn router() -> Router {
-    Router::new().route("/", any(index)).merge(health::router())
-}
-
-async fn index() -> StatusCode {
-    StatusCode::NO_CONTENT
+    Router::new()
+        .route("/", any(async || StatusCode::NO_CONTENT))
+        .merge(health::router())
 }
